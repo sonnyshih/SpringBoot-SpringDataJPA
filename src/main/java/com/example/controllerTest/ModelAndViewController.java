3f0082cfcb6d08f3d6e509bd32e8cbd528b6d190
sonny <sonnyshih@gmail.com>
2022-06-09 14:04:30 +0800

    1. add the ModelAndViewController demo

---
+++ src/main/java/com/example/controllerTest/ModelAndViewController.java
@@ -0,0 +1,36 @@
+package com.example.controllerTest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
+
+/**
+ * There are two type to display the value int the html
+ * 1. Use the Map<String, Object>
+ * 2. Use the Model
+ * */
+
+@Controller
+public class ModelAndViewController {
+
+
+    // Type 1: Use the Map<String, Object>
+    @RequestMapping("/test")
+    public ModelAndView testModelAndView(Map<String, Object> model){
+        model.put("number", 1234);
+        model.put("message", "Hello from Spring MVC");
+        return new ModelAndView("test/hello");
+    }
+
+
+    // Type 2: Use the Model
+    @RequestMapping("/test2")
+    public ModelAndView testModelAndView2(Model model){
+        model.addAttribute("number", 456789);
+        model.addAttribute("message", "Hello from Spring MVC (Model)");
+        return new ModelAndView("test/hello");
+    }
+}
