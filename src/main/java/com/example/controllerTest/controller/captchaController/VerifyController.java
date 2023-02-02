a8cc7131520a77e40118bbba157998cccf719a5f
sonnyshih <sonnyshih@gmail.com>
2023-02-02 16:59:46 +0800

    1. Develop the captcha demo.

---
+++ src/main/java/com/example/controllerTest/controller/captchaController/VerifyController.java
@@ -0,0 +1,96 @@
+package com.example.controllerTest.controller.captchaController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.OutputStream;
+
+@RestController
+@RequestMapping("verify")
+public class VerifyController {
+
+    @GetMapping("/abc")
+    public String index() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/getcode")
+    public void getCode(HttpServletResponse response, HttpServletRequest request) throws Exception {
+        // 获取到session
+        HttpSession session = request.getSession();
+        // 取到sessionid
+        String id = session.getId();
+
+        // 利用图片工具生成图片
+        // 返回的数组第一个参数是生成的验证码，第二个参数是生成的图片
+        Object[] objs = VerifyUtil.newBuilder()
+                .setWidth(200)      // 預設圖片寬度
+                .setHeight(60)      // 預設圖片高度
+                .setSize(6)         // 預設字數
+                .setLines(5)        // 預設干擾線條數
+                .setFontSize(30)    // 預設字體大小
+                .setTilt(true)      // 預設字體是否需要倾斜
+                .setThetaDegree(20) // 預設字體斜角度
+                .setBackgroundColor(Color.white) // 設定驗証碼背景顏色
+                .build()
+                .createImage();  //產生圖片
+
+        // 将验证码存入Session
+        session.setAttribute("SESSION_VERIFY_CODE_" + id, objs[0]);
+
+        // 印出驗証碼
+        System.out.println(objs[0]);
+
+//        // 设置redis值的序列化方式
+//        redisTemplate.setValueSerializer(new StringRedisSerializer());
+//        // 在redis中保存一个验证码最多尝试次数
+//        // 这里采用的是先预设一个上限次数，再以reidis decrement(递减)的方式来进行验证
+//        // 这样有个缺点，就是用户只申请验证码，不验证就走了的话，这里就会白白占用5分钟的空间，造成浪费了
+//        // 为了避免以上的缺点，也可以采用redis的increment（自增）方法，只有用户开始在做验证的时候设置值，
+//        //    超过多少次错误，就失效；避免空间浪费
+//        redisTemplate.opsForValue().set(("VERIFY_CODE_" + id), "3", 5 * 60, TimeUnit.SECONDS);
+
+        // 将图片输出给浏览器
+        BufferedImage image = (BufferedImage) objs[1];
+        response.setContentType("image/png");
+        OutputStream os = response.getOutputStream();
+        ImageIO.write(image, "png", os);
+    }
+
+    @GetMapping("/checkcode")
+    public String checkCode(String code, HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        String id = session.getId();
+
+        // 将redis中的尝试次数减一
+        String verifyCodeKey = "VERIFY_CODE_" + id;
+//        long num = redisTemplate.opsForValue().decrement(verifyCodeKey);
+
+        // 如果次数次数小于0 说明验证码已经失效
+//        if (num < 0) {
+//            return "验证码失效!";
+//        }
+
+        // 将session中的取出对应session id生成的验证码
+        String serverCode = (String) session.getAttribute("SESSION_VERIFY_CODE_" + id);
+        // 校验验证码
+        if (null == serverCode || null == code || !serverCode.toUpperCase().equals(code.toUpperCase())) {
+            return "验证码错误!";
+        }
+
+        // 验证通过之后手动将验证码失效
+//        redisTemplate.delete(verifyCodeKey);
+
+        // 这里做具体业务相关
+
+        return "验证码正确!";
+    }
+}
---
+++ src/main/java/com/example/controllerTest/controller/captchaController/VerifyUtil.java
@@ -0,0 +1,233 @@
+package com.example.controllerTest.controller.captchaController;
+
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.util.Random;
+
+/**
+ * refer to: https://www.cnblogs.com/ming-blogs/p/16271358.html
+ * */
+public class VerifyUtil {
+
+    /**
+     *  預設驗証碼字集
+     */
+    private static final char[] chars = {
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    /**
+     * 預設字符数量
+     */
+    private final Integer SIZE;
+    /**
+     * 預設干擾線數量
+     */
+    private final int LINES;
+    /**
+     * 預設圖片宽度
+     */
+    private final int WIDTH;
+    /**
+     * 預設圖片高度
+     */
+    private final int HEIGHT;
+    /**
+     * 預設字體大小
+     */
+    private final int FONT_SIZE;
+    /**
+     * 預設斜體字
+     */
+    private final boolean TILT;
+
+    private final int THETA_DEGREE;
+
+    /**
+     * 背景顏色
+     */
+    private final Color BACKGROUND_COLOR;
+
+    /**
+     * 初始化基础参数
+     *
+     * @param builder
+     */
+    private VerifyUtil(Builder builder) {
+        SIZE = builder.size;
+        LINES = builder.lines;
+        WIDTH = builder.width;
+        HEIGHT = builder.height;
+        FONT_SIZE = builder.fontSize;
+        TILT = builder.tilt;
+        THETA_DEGREE = builder.thetaDegree;
+        BACKGROUND_COLOR = builder.backgroundColor;
+    }
+
+    /**
+     * 实例化构造器对象
+     *
+     * @return
+     */
+    public static Builder newBuilder() {
+        return new Builder();
+    }
+
+    /**
+     * @return 產生驗証碼與圖片
+     * Object[0]：驗証碼字串
+     * Object[1]：驗証碼圖片。
+     */
+    public Object[] createImage() {
+        StringBuffer sb = new StringBuffer();
+        // 建立空白圖片
+        BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+
+        // 取得圖片畫筆
+        Graphics2D graphic = image.createGraphics();
+
+        //
+        graphic.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        // 設置畫筆顏色
+        graphic.setColor(BACKGROUND_COLOR);
+
+        // 繪製矩形背景
+        graphic.fillRect(0, 0, WIDTH, HEIGHT);
+
+        Random ran = new Random();
+
+        //graphic.setBackground(Color.WHITE);
+
+        // 計算每個字符佔的寬度，這裡預留一個字符的位置用於左右邊距
+        int codeWidth = WIDTH / (SIZE + 1);
+
+        // 字符所處的y軸的坐標
+        int y = HEIGHT * 3 / 4;
+
+        for (int i = 0; i < SIZE; i++) {
+            // 設置隨機顏色
+            graphic.setColor(getRandomColor());
+
+            // 初始化字體
+            Font font = new Font(null, Font.BOLD + Font.ITALIC, FONT_SIZE);
+
+            if (TILT) {
+                // 隨機傾斜角度 (-30到30度之間)
+                int theta = ran.nextInt(THETA_DEGREE);
+
+                // 隨機傾斜方向 左或者右
+                theta = (ran.nextBoolean() == true) ? theta : -theta;
+                AffineTransform affineTransform = new AffineTransform();
+                affineTransform.rotate(Math.toRadians(theta), 0, 0);
+                font = font.deriveFont(affineTransform);
+            }
+
+            // 設定字形、大小
+            graphic.setFont(font);
+
+            // 計算當前字符繪製的X軸坐標
+            int x = (i * codeWidth) + (codeWidth / 2);
+
+            // 隨機取字索引
+            int n = ran.nextInt(chars.length);
+
+            // 得到字
+            String code = String.valueOf(chars[n]);
+
+            // 畫字到圖中
+            graphic.drawString(code, x, y);
+
+            // 記錄字
+            sb.append(code);
+        }
+
+        // 畫干擾線
+        for (int i = 0; i < LINES; i++) {
+            // 設定隨機顏色
+            graphic.setColor(getRandomColor());
+
+            // 隨機畫線
+            graphic.drawLine(ran.nextInt(WIDTH), ran.nextInt(HEIGHT), ran.nextInt(WIDTH), ran.nextInt(HEIGHT));
+        }
+
+        // 返回驗証碼和圖片
+        return new Object[]{sb.toString(), image};
+    }
+
+    /**
+     * 隨機取色
+     */
+    private Color getRandomColor() {
+        Random ran = new Random();
+        Color color = new Color(ran.nextInt(256), ran.nextInt(256), ran.nextInt(256));
+        return color;
+    }
+
+    /**
+     * 构造器对象
+     */
+    public static class Builder {
+        // 預設字符数量
+        private int size = 4;
+        // 預設干擾線數量
+        private int lines = 10;
+        // 預設圖片宽度
+        private int width = 80;
+        // 預設圖片高度
+        private int height = 35;
+        // 預設字體大小
+        private int fontSize = 25;
+        // 預設斜體字
+        private boolean tilt = true;
+        private int thetaDegree = 30;
+        //背景顏色
+        private Color backgroundColor = Color.LIGHT_GRAY;
+
+        public Builder setSize(int size) {
+            this.size = size;
+            return this;
+        }
+
+        public Builder setLines(int lines) {
+            this.lines = lines;
+            return this;
+        }
+
+        public Builder setWidth(int width) {
+            this.width = width;
+            return this;
+        }
+
+        public Builder setHeight(int height) {
+            this.height = height;
+            return this;
+        }
+
+        public Builder setFontSize(int fontSize) {
+            this.fontSize = fontSize;
+            return this;
+        }
+
+        public Builder setTilt(boolean tilt) {
+            this.tilt = tilt;
+            return this;
+        }
+
+        public Builder setThetaDegree(int thetaDegree) {
+            this.thetaDegree = thetaDegree;
+            return this;
+        }
+
+        public Builder setBackgroundColor(Color backgroundColor) {
+            this.backgroundColor = backgroundColor;
+            return this;
+        }
+
+        public VerifyUtil build() {
+            return new VerifyUtil(this);
+        }
+    }
+
+}
